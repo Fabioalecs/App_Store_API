8bf333e96e31b0ef96c909e86266ba5c11fe5b98
Fabio <fabio_alex1@icloud.com>
2022-03-13 22:21:36 -0300

    Ajustes

--- App/Controllers/ProductController.php
+++ App/Controllers/ProductController.php
@@ -12,9 +12,28 @@ final class ProductController
 
     public function getProducts(Request $request, Response $response, array $args): Response
     {
-        $response = $response->withJson([
-            "message" => "Hello World"
-        ]);
+       
+
+        return $response;
+    }
+
+    public function insertProduct(Request $request, Response $response, array $args): Response
+    {
+       
+
+        return $response;
+    }
+
+    public function updateProduct(Request $request, Response $response, array $args): Response
+    {
+       
+
+        return $response;
+    }
+
+    public function deleteProduct(Request $request, Response $response, array $args): Response
+    {
+       
 
         return $response;
     }
