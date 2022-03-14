e41e29bb50002cb89cfb6b3cc00a3215cea7d492
Fabio <fabio_alex1@icloud.com>
2022-03-13 22:12:50 -0300

    Ajuste

---
+++ App/Controllers/StoreController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controllers;
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+final class StoreController 
+{
+    public function getStories(Request $request, Response $response, array $args): Response 
+    {
+
+
+        return $response;
+    }
+    
+    public function insertStore(Request $request, Response $response, array $args): Response 
+    {
+
+
+        return $response;
+    }
+    
+    public function updateStore(Request $request, Response $response, array $args): Response 
+    {
+
+
+        return $response;
+    }
+    
+    public function deleteStore(Request $request, Response $response, array $args): Response 
+    {
+        
+
+        return $response;
+    }
+
+}
