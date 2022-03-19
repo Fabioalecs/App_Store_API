852f81ce91cccc38dc95b3a57e1607b18379f3cc
Fabio <fabio_alex1@icloud.com>
2022-03-18 22:32:19 -0300

    Atualização para uso do docker

---
+++ App/Controllers/AuthController.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Controllers;
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+final class AuthController 
+{
+    public function login(Request $request, Response $response, array $args): Response
+    {
+
+        $response->getBody()
+                 ->write("teste");
+
+        return $response;
+    }
+
+}
--- routes/index.php
+++ routes/index.php
@@ -1,12 +1,17 @@
 <?php
 
-use App\Controllers\ProductController;
-use App\Controllers\StoreController;
+
+use App\Controllers\{
+    AuthController,
+    ProductController,
+    StoreController
+};
 
 use function src\slimConfiguration;
 
 $app = new \Slim\App(slimConfiguration());
 
+$app->post('/login', AuthController::class . ':login');
 
 $app->get('/store', StoreController::class . ':getStore');
 $app->post('/store', StoreController::class . ':insertStore');
