b9be8f696982079ec990cb036ca293094d2860de
Fabio <fabio_alex1@icloud.com>
2022-03-20 22:03:18 -0300

    Atualização

--- App/DAO/MySQL/GerenciadorDeLojas/ProductsDAO.php
+++ App/DAO/MySQL/GerenciadorDeLojas/ProductsDAO.php
@@ -2,6 +2,8 @@
 
 namespace App\DAO\MySQL\GerenciadorDeLojas;
 
+use App\Models\MySQL\GerenciadorDeLojas\ProductModel;
+
 class ProductsDAO extends Connection
 {
     public function __construct()
@@ -18,9 +20,41 @@ class ProductsDAO extends Connection
         return $products;
     }
 
-    public function insertProduct(): void
+    public function insertProduct(ProductModel $product): void
     {
-        
+        $stmt = $this->pdo
+            ->prepare('INSERT INTO produtos VALUES(
+                null,
+                :loja_id,
+                :nome,
+                :preco,
+                :quantidade
+                );');
+        $stmt->execute([
+            'loja_id' => $product->getStoreId(),
+            'nome' => $product->getName(),
+            'preco' => $product->getPrice(),
+            'quantidade' => $product->getAmount()  
+        ]);
     }
 
+    public function updateProduct(ProductModel $product): void
+    {
+        $stmt = $this->pdo
+            ->prepare('UPDATE produtos SET
+            nome = :nome,
+            preco = :preco,
+            quantidade = :quantidade
+        WHERE 
+            id = :id
+        ;');
+        $stmt->execute([
+            'nome' => $product->getName(),
+            'preco' => $product->getPrice(),
+            'quantidade' => $product->getAmount(),
+            'id' => $product->getId()
+        ]);
+    }
+
+    
 }
